7c4f47af41261b377bfe09c307df1f4c538d5e54
intellisr <intellisr@gmail.com>
2020-05-07 12:32:13 +0530

    finale

--- tenureTime.py
+++ tenureTime.py
@@ -8,7 +8,7 @@ target=dataset[:,11]
 from sklearn.model_selection import train_test_split
 #dataset splitting function
 
-train_data,test_data,train_target,test_target=train_test_split(data,target,test_size=0.1)
+train_data,test_data,train_target,test_target=train_test_split(data,target,test_size=0.3)
 
 #from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
@@ -22,14 +22,14 @@ algorithm.fit(train_data,train_target)
 result=algorithm.predict(test_data)
 #testing
 
-print('Actual Target:',test_target)
-print('Predicted Target:',result)
+#print('Actual Target:',test_target)
+#print('Predicted Target:',result)
 
 from sklearn.metrics import accuracy_score
 
 acc=accuracy_score(test_target,result)
 
-print('Accuracy:',acc)
+print('Accuracy:',int(acc*100),'%')
 
 import joblib
 
